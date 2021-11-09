dac8bcbbb3327559be0b7061467e7eff7c9b3129
younan nagy <younan.nagy2@gmail.com>
2021-11-09 14:51:21 +0200

    r

--- flaskr/auth/auth.py
+++ flaskr/auth/auth.py
@@ -1,12 +1,13 @@
 import json
+import os
 from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-AUTH0_DOMAIN = 'dev-4pyf7rh9.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'casting'
+AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
+ALGORITHMS = os.getenv('ALGORITHMS')
+API_AUDIENCE = os.getenv('API_AUDIENCE')
 
 
 
--- setup.sh
+++ setup.sh
@@ -8,5 +8,4 @@ export API_AUDIENCE="casting"
 export ALGORITHMS=['RS256']
 export FLASK_APP=flaskr
 export FLASK_ENV=development
-export DATABASE_URI=" postgres://xovawgjaxekusq:6feffebf70bd8caa4817ab6ed0a6567a98e67cd909743a23986456ea6a1b1636@ec2-54-160-35-196.compute-1.amazonaws.com:5432/d5amt59qcm45op"
-export NEW_VAR="Testing export"
+export DATABASE_URI="postgres://xovawgjaxekusq:6feffebf70bd8caa4817ab6ed0a6567a98e67cd909743a23986456ea6a1b1636@ec2-54-160-35-196.compute-1.amazonaws.com:5432/d5amt59qcm45op"
